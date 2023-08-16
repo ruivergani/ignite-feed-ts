2a1cb1c4346550a02d1d749d329a7456646c2ea4
ruivergani <ruiverganineto@gmail.com>
2023-08-16 14:14:24 +0100

    Extension of interface using Attributes

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,17 +2,14 @@ import styles from './Avatar.module.css';
 import { ImgHTMLAttributes } from 'react';
 
 interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{ // extende todas as propriedades do Img HTML <passa como default generics o elemento HTML>
-    hasBorder?: boolean;
-    src?: string;
-    alt?: string; // ? => makes the property optional to the component (either use or not)
+    hasBorder?: boolean; // ? => makes the property optional to the component (either use or not)
 }
 // Quando voce quer que o componente tenha as propriedades que criamos e tambem as nativas do HTML (no caso da imagem sao varias) = tem que usar as extensao de interfaces (ImgHTMLAttributes)
-export function Avatar({src, hasBorder = true, alt} : AvatarProps){
+export function Avatar({hasBorder = true,...props} : AvatarProps){
     return (
         <img 
-            src={src} 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-            alt={alt}
+            {...props} // usando o spread operator para usar todos valores dentro do props e passando como propriedade para tag image (nao precisa passar src, alt ou outra propriedade - sera automatico o valor)
         />
     )
 }
