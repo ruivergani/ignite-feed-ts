f46d2c85c27986a326d9f29fa2d1fa394294229c
ruivergani <ruiverganineto@gmail.com>
2023-08-16 13:59:21 +0100

    Extends learning concept for HTML Attributes

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,11 +1,12 @@
 import styles from './Avatar.module.css';
+import { ImgHTMLAttributes } from 'react';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{ // extende todas as propriedades do Img HTML <passa como default generics o elemento HTML>
     hasBorder?: boolean;
     src?: string;
     alt?: string; // ? => makes the property optional to the component (either use or not)
 }
-
+// Quando voce quer que o componente tenha as propriedades que criamos e tambem as nativas do HTML (no caso da imagem sao varias) = tem que usar as extensao de interfaces (ImgHTMLAttributes)
 export function Avatar({src, hasBorder = true, alt} : AvatarProps){
     return (
         <img 
